a043614f46c27859e3332154443924eb7cb87bc3
Francisco Ramirez <francisco.ramirez@groupecat.com.ar>
2019-07-02 09:48:21 -0300

    creacio componente lugar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SectorComponent } from './sector/sector.component';
+import { LugarComponent } from './lugar/lugar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SectorComponent
+    SectorComponent,
+    LugarComponent
   ],
   imports: [
     BrowserModule,
