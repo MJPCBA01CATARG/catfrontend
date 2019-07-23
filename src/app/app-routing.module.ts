d058b7e3720025422252583e9ffc984248f98dd5
Emanuel <lejiei@gmail.com>
2019-07-23 09:35:42 -0300

    Estilos Principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TurnosComponent } from './turnos/turnos.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'turnos.component', component: TurnosComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/listaempleados/listaempleados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listaempleados',
+  templateUrl: './listaempleados.component.html',
+  styleUrls: ['./listaempleados.component.css']
+})
+export class ListaempleadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
