e380bf82f3e88bf31791ebaa5a15002a4f3587df
Emanuel <lejiei@gmail.com>
2019-07-30 06:05:25 -0300

    Routing empleado, turnos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TurnosComponent } from './turnos/turnos.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
 
 
-const routes: Routes = [{ path: 'turnos', component: TurnosComponent }];
+const routes: Routes = [
+  { path: 'turnos', component: TurnosComponent },
+  { path: 'empleados', component: EmpleadosComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
